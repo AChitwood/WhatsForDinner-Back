fa7d7086a7c487229ca062b25be3b4a104d09a55
AChitwood <aschitwo@uark.edu>
2017-02-12 15:40:08 -0600

    Fixed ObjectId Reference Error

--- service/app/recipe/index.js
+++ service/app/recipe/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose'),
 	Recipe = mongoose.model("Recipe"),
-	ObjectId = mongoose.Types.ObjectId()
+	ObjectId = mongoose.Types.ObjectId
 
 exports.create = function(req, res, next) {
 	var recipeModel = new Recipe(req.body);
