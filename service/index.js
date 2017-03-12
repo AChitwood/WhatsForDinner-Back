88416fe779ec68bd964d0d8d9d23cbf90dbdba2a
Peyton Combs <peyton@combs.io>
2017-03-12 14:19:58 -0500

    authentication try 4

--- service/index.js
+++ service/index.js
@@ -27,6 +27,37 @@ server.use(restify.fullResponse());
 
 
 
+//............  Authentication
+server.use(restify.authorizationParser());
+server.use(function (req, res, next){
+  var users;
+
+    // if (/* some condition determining whether the resource requires authentication */) {
+    //    return next();
+    // }
+
+    users = {
+        foo: {
+            id: 1,
+            password: 'bar'
+        }
+    };
+
+    // Ensure that user is not anonymous; and
+    // That user exists; and
+    // That user password matches the record in the database.
+    if (req.username == 'anonymous' || !users[req.username] || req.authorization.basic.password !== users[req.username].password) {
+        // Respond with { code: 'NotAuthorized', message: '' }
+        next(new restify.NotAuthorizedError());
+    } else {
+        next();
+    }
+
+    next();
+});
+
+
+
 //............  Setup Controllers
 var controllers = {};
 controllers['ingredient'] = require('./app/ingredient/index.js');
@@ -61,42 +92,6 @@ server.get('/', function (req, res) {
   });
 })
 
-
-
-
-//............  Authentication
-server.use(restify.authorizationParser());
-server.use(function (req, res, next){
-  var users;
-
-    // if (/* some condition determining whether the resource requires authentication */) {
-    //    return next();
-    // }
-
-    users = {
-        foo: {
-            id: 1,
-            password: 'bar'
-        }
-    };
-
-    // Ensure that user is not anonymous; and
-    // That user exists; and
-    // That user password matches the record in the database.
-    if (req.username == 'anonymous' || !users[req.username] || req.authorization.basic.password !== users[req.username].password) {
-        // Respond with { code: 'NotAuthorized', message: '' }
-        next(new restify.NotAuthorizedError());
-    } else {
-        next();
-    }
-
-    next();
-});
-server.get('/ping', function (req, res, next) {
-    res.send('pong');
-
-    next();
-});
 
 
 
